1b382e521308f9d3b8914f4eea662eda1af0a89b
Your GitHub Username <your-email@example.com>
2026-06-19 23:10:40 +0600

    Connected real TMDB API

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-document.getElementById("movieGrid").innerHTML = "<h2 style='color:yellow'>Movie Grid Working</h2>";
+const API_KEY = 'Pd_QDj9s-CrkRUmNzBaCJ0sQdGGlccCaFpPSf6QFOju9IbApVlWEZQ==';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
+  .then(res => res.json())
+  .then(data => {
+    const grid = document.getElementById('movieGrid');
+    grid.innerHTML = ''; // আগের টেস্ট কোড মুছে ফেলবে
+    data.results.forEach(movie => {
+      grid.innerHTML += `
+        <div class="movie-card">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+          <div class="movie-info">
+            <h3>${movie.title}</h3>
+            <p class="rating">⭐ ${movie.vote_average}</p>
+          </div>
+        </div>`;
+    });
+  });
