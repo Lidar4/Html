ff32516afc999609d9d2a7c1df3c9ee625f7e736
Your GitHub Username <your-email@example.com>
2026-06-19 22:59:36 +0600

    Testing with dummy data

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const API_KEY = 
-Pd_QDj9s-CrkRUmNzBaCJ0sQdGGlccCaFpPSf6QFOju9IbApVlWEZQ== 
-const BASE_URL = 'https://api.themoviedb.org/3';
-
-fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => {
-    const grid = document.getElementById('movieGrid');
-    data.results.forEach(movie => {
-      grid.innerHTML += `
-        <div class="movie-card">
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-          <div class="movie-info">
-            <h3>${movie.title}</h3>
-            <p class="rating">Rating: ${movie.vote_average}</p>
-          </div>
-        </div>`;
-    });
-  });
-
+document.getElementById("movieGrid").innerHTML = `
+<div class="movie-card">
+    <img src="https://via.placeholder.com/300x450">
+    <div class="movie-info">
+        <h3>Test Movie</h3>
+        <p class="rating">⭐ 8.5</p>
+    </div>
+</div>
+`;
